1c527675fbb30e19a8b5f1f459fb766c93a1fa27
malizorba <97551928+malizorba@users.noreply.github.com>
2022-08-14 13:44:04 +0300

    Tests have been added

--- src/test/java/com/example/creditapplicationsystem/Service/impl/CreditResultServiceTest.java
+++ src/test/java/com/example/creditapplicationsystem/Service/impl/CreditResultServiceTest.java
@@ -1,14 +1,39 @@
 package com.example.creditapplicationsystem.Service.impl;
 
+import com.example.creditapplicationsystem.Model.Entity.CreditResult;
+import com.example.creditapplicationsystem.Model.Enum.ApprovalStatus;
+import com.example.creditapplicationsystem.Repository.CreditResultRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.ArrayList;
+import java.util.List;
+@ExtendWith(MockitoExtension.class)
 class CreditResultServiceTest {
+
+    @Mock
+    CreditResultRepository creditResultRepository;
+
+    @InjectMocks
+    CreditResultService creditResultService;
 
     @Test
     void getAllResult() {
+        //init
+        List<CreditResult> expectedResult = getSampleList();
+
+        //stub
+        Mockito.when(creditResultRepository.findAll()).thenReturn(expectedResult);
+
+        //then
+        List<CreditResult> actualAllResult = creditResultService.getAllResult();
+
     }
+
 
     @Test
     void getResultyById() {
@@ -16,5 +41,18 @@ class CreditResultServiceTest {
 
     @Test
     void getCreditResultByCustomerId() {
+    }
+    private List<CreditResult> getSampleList() {
+        List<CreditResult> sampleList = new ArrayList<>();
+        CreditResult creditResult = new CreditResult(1L,550.0,10000.0, ApprovalStatus.OK,null,null);
+        CreditResult creditResult1 = new CreditResult(2L,550.0,10000.0, ApprovalStatus.OK,null,null);
+        CreditResult creditResult2 = new CreditResult(3L,550.0,10000.0, ApprovalStatus.OK,null,null);
+
+        sampleList.add(creditResult);
+        sampleList.add(creditResult1);
+        sampleList.add(creditResult2);
+
+        return sampleList;
+
     }
 }
